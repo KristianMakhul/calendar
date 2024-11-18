a93409f95edba134cbef2d87e170873f99f7b14d
Kris13561 <folseorno@gmail.com>
2024-11-19 00:41:50 +0200

    fix my mistakes

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,18 +5,24 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        inter: ["Inter", "sans-serif"],
+        sans: ["Inter", "sans-serif"],
       },
       colors: {
-        "primary": "#333333",
+        primary: { 
+          DEFAULT: "#333333",
+          dark: "#1E1F26",
+        },
+        selected:{
+          red: "#FB3F4A",
+          green: "#589C5F",
+        },
+        divider:{
+          DEFAULT: "#F7F7F7",
+          dark: "#313131",
+        },
         "light-gray": "#666666",
-        "selected-red": "#FB3F4A",
-        "selected-green": "#589C5F",
-        "primary-dark": "#1E1F26",
         "secondary-dark": "#CCCCCC",
-        "divider": "#F7F7F7",
-        "divider-dark": "#313131",
-      },
+        },
     },
   },
   plugins: [],
